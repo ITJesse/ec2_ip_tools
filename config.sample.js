397ea8e1cab3bc5cc7105dd8ba7eb1de1633dea7
Jesse Zhu <jesse@itjesse.cn>
2016-04-17 01:05:47 +0800

    add config sample

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+var config = {
+  ec2: {
+    accessKeyId: "xxx",
+    secretAccessKey: "xxx",
+    region: "ap-northeast-1"
+  },
+  cloudflare: {
+    email: "abc@mail.com",
+    token: "xxx"
+  }
+}
+
+module.exports = config;
